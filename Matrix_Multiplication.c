b2126fdfcd3b1df023dc2b9746257e6ba37e4170
Anuj Kumar Sahu <59109386+anujkumar070@users.noreply.github.com>
2021-09-04 22:10:37 +0530

    C++ File

---
+++ Matrix_Multiplication.c
@@ -0,0 +1,54 @@
+// --By Anuj Kumar Sahu--
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1,n2,n3;
+    cin>>n1>>n2>>n3;
+    int a[n1][n2];
+    int b[n2][n3];
+    for(int i=0;i<n1;i++)
+    {
+        for(int j=0;j<n2;j++)
+            cin>>a[i][j];
+    }
+    for(int i=0;i<n2;i++)
+    {
+        for(int j=0;j<n3;j++)
+            cin>>b[i][j];
+    }
+
+    int c[n1][n3];
+    for(int i=0;i<n1;i++)
+    {
+        for(int j=0;j<n3;j++)
+        {
+            c[i][j]=0;
+        }
+        cout<<endl;
+
+    }
+    //--Matrix Multiplication- 2D Array--
+    for(int i=0;i<n1;i++)
+    {
+        for(int j=0;j<n3;j++)
+        {
+            for(int k=0;k<n2;k++)
+            {
+                c[i][j]+=a[i][k]*b[k][j];
+            }
+        }
+    }
+
+    for(int i=0;i<n1;i++)
+    {
+        for(int j=0;j<n3;j++)
+        {
+            cout<<c[i][j]<<" ";
+        }
+        cout<<endl;
+
+    }
+
+    return 0;
+}
